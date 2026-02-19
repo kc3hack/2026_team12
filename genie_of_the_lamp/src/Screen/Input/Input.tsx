c0950138cfbbe20c0f2e549ff50a0a387bcd1bae
Oshota501 <kakidesu1111@gmail.com>
2026-02-20 00:46:02 +0900

    fix : UIのバグ修正

--- genie_of_the_lamp/src/Screen/Input/Input.tsx
+++ genie_of_the_lamp/src/Screen/Input/Input.tsx
@@ -12,19 +12,29 @@ export interface IInputScreen extends InputScreen {
 }
 const InputScene = ({ onConfirm, children = <></> }: IInputScreen) => {
   const [message, setMessage] = useState(
-    "やあ！私はランプのお兄さんだよ！ 君の物欲を調べてあげよう！",
+    <div style={{ textAlign: "center", margin: "0px auto" }}>
+      やあ！<br></br>私はランプのお兄さんだよ！ <br></br>
+      君の物欲を調べてあげよう！
+    </div>,
   );
   const [text, setText] = useState("");
   const [showButton, setShowButton] = useState<boolean>(false);
 
   const handleClick = () => {
     setShowButton(true);
-    setMessage("これで大丈夫かな？");
+    setMessage(<div>これで大丈夫かな？</div>);
   };
 
   return (
     <div className="space-y-6 min-h-screen flex flex-col items-center justify-center bg-slate-900 text-white">
-      <div className="flex w-150 h-64 bg-gray-200 text-black px-4 py-2 rounded-2xl ml-80 text-4xl text-center items-center">
+      <div
+        className="flex w-96 h-40 bg-gray-200 text-black rounded-2xl text-3xl text-center items-center"
+        style={{
+          fontSize: "1.5rem",
+          padding: "0.5rem 1rem",
+          marginLeft: "5rem",
+        }}
+      >
         {message}
       </div>
       {!showButton && (
@@ -35,13 +45,20 @@ const InputScene = ({ onConfirm, children = <></> }: IInputScreen) => {
               setText(e.target.value);
             }}
             placeholder="ここに調べたいものを入力"
-            className="text-black bg-gray-100 rounded-md text-center w-100 h-10 ml-80"
+            className="text-black bg-gray-100 rounded-md text-center h-10"
+            style={{ width: "25rem", fontSize: "1rem", marginLeft: "5rem" }}
           />
           {/* <p>{text}</p> */}
           <div>
             <button
               onClick={handleClick}
-              className="mt-4 rounded-xl bg-cyan-300 px-8 py-3 text-lg font-semibold text-purple-900 shadow-lg transition hover:bg-yellow-300 active:scale-95 ml-200"
+              className="rounded-xl bg-cyan-300 font-semibold text-purple-900 shadow-lg transition hover:bg-yellow-300 active:scale-95"
+              style={{
+                fontSize: "1.125rem",
+                padding: "0.75rem 2rem",
+                marginTop: "1rem",
+                marginLeft: "5rem",
+              }}
             >
               決定
             </button>
@@ -50,7 +67,15 @@ const InputScene = ({ onConfirm, children = <></> }: IInputScreen) => {
       )}
       {showButton && (
         <div>
-          <p className="text-white rounded-md text-center w-100 h-10 ml-80 px-8 py-2 text-2xl">
+          <p
+            className="text-white rounded-md text-center h-10"
+            style={{
+              width: "25rem",
+              fontSize: "1.5rem",
+              padding: "0.5rem 2rem",
+              marginLeft: "5rem",
+            }}
+          >
             調べるもの：{text}
           </p>
           <button
@@ -60,7 +85,13 @@ const InputScene = ({ onConfirm, children = <></> }: IInputScreen) => {
                 name: text,
               })
             }
-            className="mt-4 rounded-xl bg-cyan-300 px-8 py-3 text-lg font-semibold text-purple-900 shadow-lg transition hover:bg-yellow-300 active:scale-95 ml-200"
+            className="rounded-xl bg-cyan-300 font-semibold text-purple-900 shadow-lg transition hover:bg-yellow-300 active:scale-95"
+            style={{
+              fontSize: "1.125rem",
+              padding: "0.75rem 2rem",
+              marginTop: "1rem",
+              marginLeft: "5rem",
+            }}
           >
             次へ
           </button>
--- genie_of_the_lamp/src/UI/Demon/Demon.tsx
+++ genie_of_the_lamp/src/UI/Demon/Demon.tsx
@@ -1,13 +1,76 @@
+import { useState } from "react";
 import kakiuchiImage from "../../Assets/kakiuchi.png";
 
+class DemonController {
+  constructor() {}
+
+  public now: number = 0;
+
+  private setPosition?: (ph: number) => void;
+
+  private ph = 0;
+
+  private interval = setInterval(() => {
+    this.now = Math.sin(this.ph) * 4;
+    if (this.setPosition) this.setPosition(this.now);
+    this.ph += 0.02;
+  }, 40);
+
+  public setPositionFunction(f: (ph: number) => void) {
+    this.setPosition = f;
+  }
+  public removeAnimation() {
+    clearInterval(this.interval);
+  }
+}
+
+const demon_controller = new DemonController();
+
 const Demon = () => {
+  const [position, setPosition] = useState(demon_controller.now);
+  demon_controller.setPositionFunction(setPosition);
+
   return (
     <div>
-      <img
-        src={kakiuchiImage}
-        alt="kakiuchi"
-        style={{ width: "40%", height: "40%" }}
-      />
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src={kakiuchiImage}
+                alt="kakiuchi"
+                style={{
+                  transform: `rotateZ(-10deg)translateY(${position + 3}rem)`,
+                  width: "10rem",
+                  height: "40rem",
+                }}
+              />
+            </td>
+            <td>
+              <img
+                src={kakiuchiImage}
+                alt="kakiuchi"
+                style={{
+                  width: "10rem",
+                  height: "40rem",
+                  transform: `translateY(${position}rem)`,
+                }}
+              />
+            </td>
+            <td>
+              <img
+                src={kakiuchiImage}
+                alt="kakiuchi"
+                style={{
+                  width: "10rem",
+                  height: "40rem",
+                  transform: `rotateZ(10deg)translateY(${position + 3}rem)`,
+                }}
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
